e040563bcc7a1062e43fd1fa6670d6bf5101aaf0
Brenna Neeland <123501165+bneeland3@users.noreply.github.com>
2023-12-11 20:57:22 -0700

    Update test.sh
    updated test.sh for sssh testing

--- test/Functional_test/test.sh
+++ test/Functional_test/test.sh
@@ -1,18 +1,15 @@
-#!/bin/bash/env python
-
-cd ../../src # Change directory to ../src
-parent_dir='$(pwd)' # parent_dir is defined as the print working directory.
-file_name= 'download_test_data.py' # Creates a variable name for the file name.
-file_path='${parent_dir}/${file_name}' # The file path is defined as the dir/file_name.
-
-# echo 'File Path: ${file_path}' # This prints our file path
+#!/bin/bash
 
 # Lines 11 and 13 are for the Stupid Simple Bash Testing.
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 
 source ssshtest
 
-# run basic_assert python "file_path"
-# assert_equal $file_name $( ls $file_name )
+cd ../../src # Change directory to ../src
+pip install gdown
 
-# run basic_obs python python 'file_path'
+run test_basic_download_code python download_test_data.py 
+assert_exit_code 0
+
+run test_basic_samples python sample_names.py
+assert_exit_code 0
