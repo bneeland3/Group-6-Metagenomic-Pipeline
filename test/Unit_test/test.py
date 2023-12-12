b595f8b9e62bf3a1347dc17469b689a7cd5a6f00
Brenna Neeland <brne3762@colorado.edu>
2023-12-12 04:14:45 +0000

    test.py changed

--- test/Unit_test/test.py
+++ test/Unit_test/test.py
@@ -3,20 +3,25 @@ import unittest
 
 sys.path.insert(0, '../../src')  # noqa
 
+
 def read_sample_names(filename):
     with open(filename, 'r') as file:
         return [line.strip().split('.')[0].split('_')[0] for line in file]
+
 
 class TestReadSampleNames(unittest.TestCase):
     def test_read_sample_names(self):
         # Prepare a test sample_names.txt file with some sample names
         with open("sample_names.txt", "w") as file:
-            file.write("sample1_file.fastq\nsample2_file.fastq\nsample3_file.fastq")
+            file.write(
+                '''sample1_file.fastq\
+                nsample2_file.fastq\nsample3_file.fastq'''
+            )
 
         # Test if read_sample_names function extracts sample names correctly
         samples = read_sample_names("sample_names.txt")
         self.assertEqual(samples, ["sample1", "sample2", "sample3"])
 
+
 if __name__ == "__main__":
     unittest.main()
-       
